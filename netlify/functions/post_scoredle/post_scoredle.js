191a75c24faafbd86c3cf47fbc0f0e5aa8cd30ab
brendan <alber123@mail.chapman.edu>
2022-03-06 00:16:00 -0800

    test move mongodb client code into each function

--- netlify/functions/post_scoredle/post_scoredle.js
+++ netlify/functions/post_scoredle/post_scoredle.js
@@ -49,7 +49,32 @@ const postScoredle = async (db, document) => {
   };
 };
 
-const clientPromise = require("../../mongodb-client/mongodb-client");
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+
+const uri = process.env.VITE_MONGO_URI;
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+
+let client;
+let clientPromise;
+
+if (process.env.VITE_NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+
+  clientPromise = global._mongoClientPromise;
+} else {
+  // Else in production it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 exports.handler = async function (event, context) {
   if (env === "prod") {
