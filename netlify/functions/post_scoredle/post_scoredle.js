e8bac2126a6ee1708af8cedf23fd286cea3b4f60
brendan <alber123@mail.chapman.edu>
2022-12-01 16:54:45 -0800

    add optional chaining  fix

--- netlify/functions/post_scoredle/post_scoredle.js
+++ netlify/functions/post_scoredle/post_scoredle.js
@@ -9,101 +9,108 @@ const localDbUri = process.env.LOCAL_JSON_DB;
 const admin = process.env.VITE_ADMIN;
 
 const postScoredle = async (db, document) => {
-  let result;
-  if (document.user === admin) {
-    result = await db
-      .collection(env === "prod" ? collectionname : stagingCollectionName)
-      .updateOne(
-        { date: document.date },
-        {
-          $set: {
-            worldle: document.worldle ?? "",
-            wordle: document.wordle ?? "",
-            word: document.word ?? "",
-            svg: document.svg ?? "",
-            country: document.country ?? "",
-            region: document.region ?? "",
-            state: document.state ?? "",
-            statele: document.statele ?? "",
-            state_svg: document.state_svg ?? "",
-            de_state: document.de_state ?? "",
-            de_svg: document.de_svg ?? "",
-          },
-        },
-        {
-          upsert: true,
-        }
-      );
-  }
+    let result;
+    if (document.user === admin) {
+        result = await db
+            .collection(env === "prod" ? collectionname : stagingCollectionName)
+            .updateOne(
+                { date: document.date },
+                {
+                    $set: {
+                        worldle: document.worldle ?? "",
+                        wordle: document.wordle ?? "",
+                        word: document.word ?? "",
+                        svg: document.svg ?? "",
+                        country: document.country ?? "",
+                        region: document.region ?? "",
+                        state: document.state ?? "",
+                        statele: document.statele ?? "",
+                        state_svg: document.state_svg ?? "",
+                        de_state: document.de_state ?? "",
+                        de_svg: document.de_svg ?? "",
+                    },
+                },
+                {
+                    upsert: true,
+                }
+            );
+    }
 
-  const user = document.user;
+    const user = document.user;
 
-  let userScoreObject = document.scores?.filter((uso) => uso.name === user)[0];
+    let userScoreObject = document.scores?.filter(
+        (uso) => uso.name === user
+    )[0];
 
-  const todaysGamesObject = await db
-    .collection(env === "prod" ? collectionname : stagingCollectionName)
-    .findOne({ date: document.date });
+    const todaysGamesObject = await db
+        .collection(env === "prod" ? collectionname : stagingCollectionName)
+        .findOne({ date: document.date });
 
-  const { scores: latestScoresFromDB } = todaysGamesObject ?? {};
+    const { scores: latestScoresFromDB } = todaysGamesObject ?? {};
 
-  const filteredUserScoreObjFromDB = latestScoresFromDB?.filter(
-    (s) => s.name === user
-  )[0];
+    const filteredUserScoreObjFromDB = latestScoresFromDB?.filter(
+        (s) => s.name === user
+    )[0];
 
-  const newUserScoreObj = {
-    ...filteredUserScoreObjFromDB,
-    ...userScoreObject,
-  };
+    const newUserScoreObj = {
+        ...filteredUserScoreObjFromDB,
+        ...userScoreObject,
+    };
 
-  const scoresSansUsersObjList = latestScoresFromDB?.filter(
-    (s) => s.name !== user
-  );
-
-  const newScores =
-    Object.keys(newUserScoreObj).length > 0
-      ? [...(scoresSansUsersObjList ?? []), newUserScoreObj]
-      : [];
-
-  let result2 = await db
-    .collection(env === "prod" ? collectionname : stagingCollectionName)
-    .updateOne(
-      { date: document.date },
-      {
-        $set: {
-          scores: newScores,
-        },
-      },
-      { upsert: true }
+    const scoresSansUsersObjList = latestScoresFromDB?.filter(
+        (s) => s.name !== user
     );
 
-  let year_month = new Date().getFullYear() + "/" + (new Date().getMonth() + 1);
+    const newScores =
+        Object.keys(newUserScoreObj).length > 0
+            ? [...(scoresSansUsersObjList ?? []), newUserScoreObj]
+            : [];
 
-  await db.collection(errorLogCollection).updateOne(
-    { year_month: year_month },
-    {
-      $push: {
-        logs: {
-          entry: `${document.user} ~ [${new Date()}] - ${JSON.stringify(
-            result
-          )} | ${JSON.stringify(result2)} | ${JSON.stringify(document)}`,
+    let result2 = await db
+        .collection(env === "prod" ? collectionname : stagingCollectionName)
+        .updateOne(
+            { date: document.date },
+            {
+                $set: {
+                    scores: newScores,
+                },
+            },
+            { upsert: true }
+        );
+
+    let year_month =
+        new Date().getFullYear() + "/" + (new Date().getMonth() + 1);
+
+    await db.collection(errorLogCollection).updateOne(
+        { year_month: year_month },
+        {
+            $push: {
+                logs: {
+                    entry: `${
+                        document.user
+                    } ~ [${new Date()}] - ${JSON.stringify(
+                        result
+                    )} | ${JSON.stringify(result2)} | ${JSON.stringify(
+                        document
+                    )}`,
+                },
+            },
         },
-      },
-    },
-    {
-      upsert: true,
-    }
-  );
+        {
+            upsert: true,
+        }
+    );
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      text: "added or updated document to/in collection!",
-      result,
-    }),
-  };
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            text: "added or updated document to/in collection!",
+            result,
+        }),
+    };
 };
 
 const { MongoClient } = require("mongodb");
@@ -111,66 +118,66 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.VITE_MONGO_URI;
 
 const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
 };
 
 let client;
 let clientPromise;
 
 if (process.env.VITE_NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options);
+        global._mongoClientPromise = client.connect();
+    }
 
-  clientPromise = global._mongoClientPromise;
+    clientPromise = global._mongoClientPromise;
 } else {
-  // Else in production it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+    // Else in production it's best to not use a global variable.
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
 }
 
 exports.handler = async function (event, context) {
-  if (env === "prod" || env === "staging") {
-    const client = await clientPromise;
+    if (env === "prod" || env === "staging") {
+        const client = await clientPromise;
 
-    const db = await client.db(dbname);
+        const db = await client.db(dbname);
 
-    return postScoredle(db, JSON.parse(event.body));
-  }
-
-  if (env === "development") {
-    const { readFile, writeFile } = require("fs/promises");
-
-    let newScoredleDateObject = JSON.parse(event.body);
-
-    console.log({ newScoredleDateObject });
-
-    const jsondb = JSON.parse(await readFile(new URL(localDbUri)));
-
-    let index = jsondb.findIndex(
-      (dateObject) => dateObject.date === newScoredleDateObject.date
-    );
-
-    if (index > -1) {
-      jsondb[index] = newScoredleDateObject;
+        return postScoredle(db, JSON.parse(event.body));
     }
 
-    if (index === -1) {
-      jsondb.push(newScoredleDateObject);
+    if (env === "development") {
+        const { readFile, writeFile } = require("fs/promises");
+
+        let newScoredleDateObject = JSON.parse(event.body);
+
+        console.log({ newScoredleDateObject });
+
+        const jsondb = JSON.parse(await readFile(new URL(localDbUri)));
+
+        let index = jsondb.findIndex(
+            (dateObject) => dateObject.date === newScoredleDateObject.date
+        );
+
+        if (index > -1) {
+            jsondb[index] = newScoredleDateObject;
+        }
+
+        if (index === -1) {
+            jsondb.push(newScoredleDateObject);
+        }
+
+        await writeFile(new URL(localDbUri), JSON.stringify(jsondb));
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                text: "successfully updated db ✅",
+            }),
+        };
     }
-
-    await writeFile(new URL(localDbUri), JSON.stringify(jsondb));
-
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text: "successfully updated db ✅",
-      }),
-    };
-  }
 };
